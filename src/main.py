55300b66fdec3dfcf485952b9be40b2111dde43c
Nagatuki <naoki.sepcenter@gmail.com>
2022-07-25 19:27:16 +0900

    refactor: move make_gif in main to utils

--- src/main.py
+++ src/main.py
@@ -2,37 +2,10 @@ import cv2
 import lmfit
 import matplotlib.pyplot as plt
 import numpy as np
-from PIL import Image
 
 import filter
 from fitting import ModelFitting
-from utils import mkdir
-
-
-def make_gif():
-    file_paths = ["./HQ/frap{:0>2}.tiff".format(i + 1) for i in range(20)]
-
-    # file check
-    for file_path in file_paths:
-        if os.path.isfile(file_path):
-            continue
-
-        print(file_path, "is not a file.")
-        return
-
-    imes = []
-    for file_path in file_paths:
-        im = Image.open(file_path).convert("P")
-        imes.append(im)
-
-    imes[0].save(
-        "./HQ/all.gif",
-        save_all=True,
-        append_images=imes[1:],
-        optimize=False,
-        duration=200,
-        loop=0,
-    )
+from utils import make_gif, mkdir
 
 
 def convert_tiff_to_png():
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import os
 
+from PIL import Image
+
 
 def mkdir(path):
     try:
@@ -11,3 +13,29 @@ def mkdir(path):
 
     except Exception as e:
         raise e
+
+
+def make_gif():
+    file_paths = ["./HQ/frap{:0>2}.tiff".format(i + 1) for i in range(20)]
+
+    # file check
+    for file_path in file_paths:
+        if os.path.isfile(file_path):
+            continue
+
+        print(file_path, "is not a file.")
+        return
+
+    imes = []
+    for file_path in file_paths:
+        im = Image.open(file_path).convert("P")
+        imes.append(im)
+
+    imes[0].save(
+        "./HQ/all.gif",
+        save_all=True,
+        append_images=imes[1:],
+        optimize=False,
+        duration=200,
+        loop=0,
+    )
